56dd9b8c69a3cde692aa18d73c3ef986148de945
luoa <luoa@uw.edu>
2019-03-12 01:20:03 -0700

    completed but not debugged sliding window

--- sliding_window.sv
+++ sliding_window.sv
@@ -1,5 +1,5 @@
 module sliding_window 
-#(parameter KERNEL_SIZE = 3, ROW_WIDTH= 100) 
+#(parameter KERNEL_SIZE = 3, ROW_WIDTH=640) 
 (
 input logic clk, reset,
 input logic [7:0] pixel_in,
@@ -15,8 +15,8 @@ output logic [7:0] buffer [KERNEL_SIZE - 1:0][KERNEL_SIZE - 1:0]
 	
 	// reset logic
 	generate
-		for (row = 0; row < KERNEL_SIZE; row += 1) begin: clear_buffer1
-			for (col = 0; col < KERNEL_SIZE; col += 1) begin: clear_buffer2
+		for (row = 0; row < KERNEL_SIZE; row += 1) begin: main_buffer_row
+			for (col = 0; col < KERNEL_SIZE; col += 1) begin: main_buffer_col
 				always_ff @(posedge clk) 
 					if(reset) begin 
 						buffer[row][col] <= 0; 
@@ -33,14 +33,16 @@ output logic [7:0] buffer [KERNEL_SIZE - 1:0][KERNEL_SIZE - 1:0]
 		end
 	endgenerate
 	
+	genvar counter_gen, row_gen;
 	generate
-		for (row = 0; row <= ROW_WIDTH - KERNEL_SIZE; row += 1) begin: clear_pass_by_buffers
-			always_ff @(posedge clk) 
-				if(reset) begin pass_by_buffer[row] <= 0; end
-				else begin
-					// TODO: fix pass in bufferes
-					pass_by_buffer[row] <= 2;
-				end
+		for (counter_gen = 0; counter_gen <= ROW_WIDTH - KERNEL_SIZE; counter_gen += 1) begin: pass_by_buffers_entries
+			for (row_gen = 1; row_gen < KERNEL_SIZE - 1; row_gen += 1) begin: pass_by_buffers_row
+				always_ff @(posedge clk) 
+					if(reset) begin pass_by_buffer[counter_gen] <= 0; end
+					else begin
+							pass_by_buffer[counter_gen][(row_gen) * 8 - 1:(row_gen - 1) * 8] <= buffer[row_gen][0];
+					end
+			end
 		end
 	endgenerate
 endmodule
