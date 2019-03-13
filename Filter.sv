b771a5665794959f94d6c2bee55fc1089cd2080c
Andrew Z. Luo <luoa@uw.edu>
2019-03-12 20:34:04 -0700

    add identity filter

--- Filter.sv
+++ Filter.sv
@@ -44,6 +44,15 @@ module Filter #(parameter WIDTH = 640, parameter HEIGHT = 480)
 	localparam PRECISION_BITS = 16;
 	localparam SCREEN_DELAY = 1;
 	
+/*
+	module sliding_window 
+#(parameter KERNEL_SIZE = 3, ROW_WIDTH=640, WORD_SIZE = 8) 
+(
+input logic clk, reset,
+input logic signed [WORD_SIZE - 1:0] pixel_in,
+output logic signed [WORD_SIZE - 1:0] buffer [KERNEL_SIZE - 1:0][KERNEL_SIZE - 1:0]
+);*/
+
 	// Simple graphics hack
 	logic [27:0] VGA_BUFFER;
 	logic [27:0] VGA_OUT;	
@@ -57,14 +66,23 @@ module Filter #(parameter WIDTH = 640, parameter HEIGHT = 480)
 	// grayscale
 	logic signed [PRECISION_BITS - 1:0] grayscale16;
 	logic [7:0] grayscale;
-	always_comb begin
+	always_ff @(posedge VGA_CLK) begin
 		grayscale16 = VGA_OUT[27:20] / 4 + VGA_OUT[19:12] / 8 * 5  + VGA_OUT[11:4] / 10;
 		grayscale = grayscale16[7:0];
 	end
 	
+	logic signed [15:0] buffer_3 [2:0][2:0];
+	logic signed [15:0] identity_kernel [2:0][2:0];
+	logic signed [15:0] identity_out;
+	
+	// sliding window operators
+	sliding_window #(3, 640, 16) kernel_in_3 (.clk(VGA_CLK), .pixel_in(grayscale16), .buffer(buffer_3));
+	kernel_convolution #(3, 16) identity_convolve(.buffer_in(buffer_3), .kernel_in(identity_kernel), .ans(identity_out));
+	
 	always_ff @(posedge VGA_CLK) begin
 		VGA_OUT <= VGA_BUFFER;
 		if (SW[0]) VGA_OUT[27:4] = {grayscale, grayscale, grayscale};
+		if (SW[1]) VGA_OUT[27:4] = {identity_out[7:0], identity_out[7:0], identity_out[7:0]};
 	end
 	
 	// set HEXES later
