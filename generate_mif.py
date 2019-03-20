fa832c1f351d7ed1fee9054f6778e0fe24c27651
luoa <luoa@uw.edu>
2019-03-19 22:33:15 -0700

    genereate mif script()

---
+++ generate_mif.py
@@ -0,0 +1,7 @@
+def generate_square_8():
+    for i in range(256):
+        print(str(i) + ' : ' + str(i * i) + ';')
+
+def generate_square_root_16():
+    for i in range (65536):
+        print(str(i) + ' : ' + str(int(i ** 0.5)) + ';')
